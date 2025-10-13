abafa61286a173eb1605efcde657699a71a874cb
anton <jappinen.anton@protonmail.com>
2025-10-13 16:09:38 +0200

    feat(api): fetch and log functions added

---
+++ api/wheaterApi.ts
@@ -0,0 +1,48 @@
+
+
+const API_KEY = "611037c86a992e8fa86891a533525e9b"
+
+export interface WheatherResult {
+	temp: number;
+	description: string;
+	icon: string;
+	cityName: string;
+}
+const buildUrlByCity = (city: string) =>
+	`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+
+const buildUrlByCoords = (lat: number, lon: number) =>
+	`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+
+export const getWheater = async (
+	opts: { city?: string, lat?: number, lon?: number },
+): Promise<WheatherResult> => {
+	let url: string = ""
+	if (opts.city == null && (opts.lat != null && opts.lon != null)) {
+		url = buildUrlByCoords(opts.lat, opts.lon)
+	} else if(opts.city != null && (opts.lat == null && opts.lon == null)) {
+			url = buildUrlByCity(opts.city)
+	}else {
+		throw new Error("Needs lat & lon or a city name");
+	}
+
+	const res = await fetch(url)
+
+	// console.log(res);
+	if (!res.ok) {
+		throw new Error(`Error from api: ${res.status}: ${res.statusText}`)
+	}
+
+	const json = await res.json()
+	const result: WheatherResult =  {
+		temp: json.main.temp,
+		description: json.weather[0].description,
+		icon: json.weather[0].icon,
+		cityName: json.name
+
+	}
+
+	return result
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,17 @@
 import { text } from 'node:stream/consumers';
 import { Text, View, StyleSheet } from 'react-native'
+import { getWheater, WheatherResult } from '@/api/wheaterApi';
 
+async function logRes() {
+	const weather: WheatherResult = await getWheater({city:"London"})
+	console.log(weather.cityName)
+	console.log(weather.icon)
+	console.log(weather.temp)
+	console.log(weather.description)
+	
+}
 export default function HomeScreen() {
+	logRes()
   return (
 		<View style={styles.container}>
 				<Text style={styles.text}>Homescreen </Text>
