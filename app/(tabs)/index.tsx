c0c83adfcc1e4beb6ca07cdb9c42c86f1542b051
anton <jappinen.anton@protonmail.com>
2025-10-25 15:16:01 +0200

    feat: dismissing keyboard on press outside it

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,6 @@ import { useTemp } from "@/hooks/use-temperature";
 import { useThemeColor } from "@/hooks/use-theme-color";
 import { loadSettings } from "@/utils/loadSettings";
 import React, { useEffect, useState } from "react";
-import { TouchableOpacity } from "react-native";
 import {
   ActivityIndicator,
   Button,
@@ -10,6 +9,9 @@ import {
   Text,
   TextInput,
   View,
+  TouchableOpacity,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { WeatherCard } from "@/components/weatherCard";
 import { useWeather } from "@/stores/weatherStore";
@@ -55,59 +57,61 @@ export default function HomeScreen() {
   };
 
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={[styles.input, { color: textColor }]}
-          placeholder="Search for a city"
-          placeholderTextColor={textColor}
-          value={city}
-          onChangeText={setCity}
-          onSubmitEditing={handleSearch}
-          returnKeyType="search"
-        />
-        <Button title="search" onPress={handleSearch} />
-      </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={[styles.container, { backgroundColor }]}>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={[styles.input, { color: textColor }]}
+            placeholder="Search for a city"
+            placeholderTextColor={textColor}
+            value={city}
+            onChangeText={setCity}
+            onSubmitEditing={handleSearch}
+            returnKeyType="search"
+          />
+          <Button title="search" onPress={handleSearch} />
+        </View>
 
-      {hasError ? (
-        <View style={styles.errorContainer}>
-          <Text style={styles.errorText}>{error}</Text>
-          <Text style={{ color: textColor }}>
-            Controll spelling or try use location instead
-          </Text>
-          <TouchableOpacity
-            onPress={fetchWeatherByCoords}
-            style={styles.locationButton}
-          >
-            <IconSymbol name="location.app.fill" color="tintColor" />
-          </TouchableOpacity>
-        </View>
-      ) : isLoading ? (
-        <ActivityIndicator
-          size="large"
-          color={tintColor}
-          style={styles.loading}
-        />
-      ) : weather ? (
-        <WeatherCard
-          weather={weather}
-          temp={convertTemp(weather.temp)}
-          unit={isCelsius ? "celsius" : "fahrenheit"}
-        />
-      ) : (
-        <View style={styles.emptyContainer}>
-          <Text style={[styles.emptyText, { color: textColor }]}>
-            Search for a city or use your location
-          </Text>
-          <TouchableOpacity
-            onPress={fetchWeatherByCoords}
-            style={styles.locationButton}
-          >
-            <IconSymbol name="location.app.fill" color="tintColor" />
-          </TouchableOpacity>
-        </View>
-      )}
-    </View>
+        {hasError ? (
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>{error}</Text>
+            <Text style={{ color: textColor }}>
+              Controll spelling or try use location instead
+            </Text>
+            <TouchableOpacity
+              onPress={fetchWeatherByCoords}
+              style={styles.locationButton}
+            >
+              <IconSymbol name="location.app.fill" color="tintColor" />
+            </TouchableOpacity>
+          </View>
+        ) : isLoading ? (
+          <ActivityIndicator
+            size="large"
+            color={tintColor}
+            style={styles.loading}
+          />
+        ) : weather ? (
+          <WeatherCard
+            weather={weather}
+            temp={convertTemp(weather.temp)}
+            unit={isCelsius ? "celsius" : "fahrenheit"}
+          />
+        ) : (
+          <View style={styles.emptyContainer}>
+            <Text style={[styles.emptyText, { color: textColor }]}>
+              Search for a city or use your location
+            </Text>
+            <TouchableOpacity
+              onPress={fetchWeatherByCoords}
+              style={styles.locationButton}
+            >
+              <IconSymbol name="location.app.fill" color="tintColor" />
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
